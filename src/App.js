d875fba032334d0cbcb990d9c02cbc115631d74b
mmarionmae <marionmaemangubat@gmail.com>
2022-10-14 01:13:16 +0800

    themes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import GlobalStyles from './styles/GlobalStyles'
+import {ThemeProvider} from 'styled-components';
+import {light} from './styles/Themes'
+
+
 
 function App() {
   return (
     <>
+    <GlobalStyles />
+    <ThemeProvider theme={light}>
       App File
+    </ThemeProvider>
+     
     </>
   );
 }
